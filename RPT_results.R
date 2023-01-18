8f9ce5d7f76f08243b13c6a9c37552e6f2e8509b
abhinavananddwivedi <abhinavananddwivedi@gmail.com>
2023-01-18 21:53:21 +0530

    functionalizing... but not very successful yet

--- RPT_results.R
+++ RPT_results.R
@@ -16,19 +16,106 @@ data_RPT <- readr::read_csv('2011_2022_CMIE_Data.csv',
 
 data_RPT_2 <- data_RPT %>%
   dplyr::select(co_code, year, NIC_2, BG, starts_with('RP_'),
-                total_assets:pbdita)
+                total_assets:pbdita) %>%
+  dplyr::rename('RPT_SALES' = RP_SALES,
+                'RPT_LOANS' = RP_LOANS)
 
-# Computing aggregate RPT---note that it includes double-counting
 
-## With double counting
- # data_RPT_2 <- data_RPT_2 %>%
- #  dplyr::mutate('RPT_agg' = RP_SALES+RP_PURCH+RP_FAINVSALES+RP_FAINVPURCH+
- #                  RP_LOANS+RP_BORROWINGS+RP_GUARGIV+RP_GUARTKN+RP_DOS+RP_INVESTEE)
+########## Computing aggregate RPT ############
 
 ## Without [?] double counting
 # Defining aggregate RPT = RPT sales+loans to avoid double counting
 data_RPT_2 <- data_RPT_2 %>%
-  dplyr::mutate('RPT_agg' = RP_SALES+RP_LOANS)
+  dplyr::mutate('RPT_agg' = RPT_SALES + RPT_LOANS)
+
+##################################################################
+############ Function declarations ###############################
+##################################################################
+
+### Aggregate RPT summary stats
+func_summ_rpt_agg <- function(df)
+{
+  temp_vec <- df$RPT_agg
+  summ_rpt_agg <- c('min' = min(temp_vec, na.rm = T),
+                    'mean' = mean(temp_vec, na.rm = T),
+                    'median' = median(temp_vec, na.rm = T),
+                    'std' = sd(temp_vec, na.rm = T),
+                    'iqr' = IQR(temp_vec, na.rm = T),
+                    'max' = max(temp_vec, na.rm = T),
+                    quantile(temp_vec, c(0.1,0.25,0.75,0.90,0.95,0.99),
+                             na.rm = T))
+  
+  return(summ_rpt_agg)
+}
+
+### RPT Sales summary stats
+func_summ_rpt_sales <- function(df)
+{
+  temp_vec <- df$RPT_SALES
+  summ_rpt_sales <- c('min' = min(temp_vec, na.rm = T),
+                    'mean' = mean(temp_vec, na.rm = T),
+                    'median' = median(temp_vec, na.rm = T),
+                    'std' = sd(temp_vec, na.rm = T),
+                    'iqr' = IQR(temp_vec, na.rm = T),
+                    'max' = max(temp_vec, na.rm = T),
+                    quantile(temp_vec, c(0.1,0.25,0.75,0.90,0.95,0.99),
+                             na.rm = T))
+  
+  return(summ_rpt_sales)
+}
+
+### RPT loans summary stats
+func_summ_rpt_loans <- function(df)
+{
+  temp_vec <- df$RPT_LOANS
+  summ_rpt_sales <- c('min' = min(temp_vec, na.rm = T),
+                      'mean' = mean(temp_vec, na.rm = T),
+                      'median' = median(temp_vec, na.rm = T),
+                      'std' = sd(temp_vec, na.rm = T),
+                      'iqr' = IQR(temp_vec, na.rm = T),
+                      'max' = max(temp_vec, na.rm = T),
+                      quantile(temp_vec, c(0.1,0.25,0.75,0.90,0.95,0.99),
+                               na.rm = T))
+  
+  return(summ_rpt_sales)
+}
+
+
+### Nesting data based on characteristics ###
+
+# Years based nesting
+data_RPT_nest_TS <- data_RPT_2 %>%
+  tidyr::nest(data = -year)
+
+### Summary stats
+data_RPT_nest_TS <- data_RPT_nest_TS %>%
+  dplyr::mutate(summ_rpt_agg_ts = purrr::map(data, func_summ_rpt_agg),
+                summ_rpt_sales_ts = purrr::map(data, func_summ_rpt_sales),
+                summ_rpt_loans_ts = purrr::map(data, func_summ_rpt_loans))
+ 
+# Extract summary stats by year
+
+
+# Industry based nesting
+data_RPT_nest_ind <- data_RPT_2 %>%
+  tidyr::nest(data = -NIC_2)
+
+# BG based nesting
+data_RPT_nest_BG <- data_RPT_2 %>%
+  tidyr::nest(data = -BG)
+
+#################################################
+######## Descriptive stats computation ##########
+#################################################
+
+func_summ_TS_ind_BG <- function(tib)
+{
+  tib_TS <- median(tib, na.rm = T)
+  
+  return(tib_2)
+}
+
+### Based on years
 
 # Time series data summary
 data_RPT_summary_TS <- data_RPT_2 %>%
@@ -87,6 +174,13 @@ data_RPT_summary_ind <- data_RPT_2 %>%
             'kurt' = moments::kurtosis(RPT_agg, na.rm = T)
   )
 
+# How many observations for each industry classification (NIC_2)
+ind_freq <- data_RPT_2 %>%
+  dplyr::group_by(NIC_2) %>%
+  dplyr::count() %>%
+  dplyr::arrange(desc(n)) %>%
+  dplyr::filter(n >= 400)
+
 data_RPT_summary_ind_top10 <- data_RPT_summary_ind %>%
   dplyr::filter(NIC_2 %in% ind_freq$NIC_2)
 
@@ -109,10 +203,136 @@ data_RPT_summary_BG <- data_RPT_2 %>%
             'kurt' = moments::kurtosis(RPT_agg, na.rm = T)
   )
 
+# Empirical distribution function for BG vs non BG RPT
 plot_ECDF_RPT_agg_BG <- ggplot(data_RPT_2, 
                                aes(y = RPT_agg, group = BG, color = BG)) +
   stat_ecdf(geom = 'step', linewidth = 0.7) +
   labs(x = 'Quantiles', y = 'Empirical cumulative distribution function')
+
+#######################################
+### Descriptive stats with RPT type ###
+#######################################
+
+###### SALES ##########
+
+### Yearly ###
+
+data_rpt_sales_summary_TS <- data_RPT_2 %>%
+  dplyr::group_by(year) %>%
+  dplyr::summarise('min' = min(RPT_SALES, na.rm = T),
+            'P10' = quantile(RPT_SALES, 0.10, na.rm = T),
+            'Q1' = quantile(RPT_SALES, 0.25, na.rm = T),
+            'med' = quantile(RPT_SALES, 0.50, na.rm = T),
+            'iqr' = IQR(RPT_SALES, na.rm =T),
+            'mean' = mean(RPT_SALES, na.rm = T),
+            'std_dev' = sd(RPT_SALES, na.rm = T),
+            'Q3' = quantile(RPT_SALES, 0.75, na.rm = T),
+            'P90' = quantile(RPT_SALES, 0.90, na.rm = T),
+            'P95' = quantile(RPT_SALES, 0.95, na.rm = T),
+            'P99' = quantile(RPT_SALES, 0.99, na.rm = T),
+            'max' = max(RPT_SALES, na.rm = T),
+            'skew' = moments::skewness(RPT_SALES, na.rm = T),
+            'kurt' = moments::kurtosis(RPT_SALES, na.rm = T))
+
+### By industry classification ###
+
+data_rpt_sales_summary_ind <- data_RPT_2 %>%
+  dplyr::group_by(NIC_2) %>%
+  dplyr::summarise('min' = min(RPT_SALES, na.rm = T),
+                   'P10' = quantile(RPT_SALES, 0.10, na.rm = T),
+                   'Q1' = quantile(RPT_SALES, 0.25, na.rm = T),
+                   'med' = quantile(RPT_SALES, 0.50, na.rm = T),
+                   'iqr' = IQR(RPT_SALES, na.rm =T),
+                   'mean' = mean(RPT_SALES, na.rm = T),
+                   'std_dev' = sd(RPT_SALES, na.rm = T),
+                   'Q3' = quantile(RPT_SALES, 0.75, na.rm = T),
+                   'P90' = quantile(RPT_SALES, 0.90, na.rm = T),
+                   'P95' = quantile(RPT_SALES, 0.95, na.rm = T),
+                   'P99' = quantile(RPT_SALES, 0.99, na.rm = T),
+                   'max' = max(RPT_SALES, na.rm = T),
+                   'skew' = moments::skewness(RPT_SALES, na.rm = T),
+                   'kurt' = moments::kurtosis(RPT_SALES, na.rm = T))
+
+### By BG classification ###
+
+data_rpt_sales_summary_BG <- data_RPT_2 %>%
+  dplyr::group_by(BG) %>%
+  dplyr::summarise('min' = min(RPT_SALES, na.rm = T),
+                   'P10' = quantile(RPT_SALES, 0.10, na.rm = T),
+                   'Q1' = quantile(RPT_SALES, 0.25, na.rm = T),
+                   'med' = quantile(RPT_SALES, 0.50, na.rm = T),
+                   'iqr' = IQR(RPT_SALES, na.rm =T),
+                   'mean' = mean(RPT_SALES, na.rm = T),
+                   'std_dev' = sd(RPT_SALES, na.rm = T),
+                   'Q3' = quantile(RPT_SALES, 0.75, na.rm = T),
+                   'P90' = quantile(RPT_SALES, 0.90, na.rm = T),
+                   'P95' = quantile(RPT_SALES, 0.95, na.rm = T),
+                   'P99' = quantile(RPT_SALES, 0.99, na.rm = T),
+                   'max' = max(RPT_SALES, na.rm = T),
+                   'skew' = moments::skewness(RPT_SALES, na.rm = T),
+                   'kurt' = moments::kurtosis(RPT_SALES, na.rm = T))
+
+
+###### LOANS ##########
+
+### Yearly ###
+
+data_rpt_loans_summary_TS <- data_RPT_2 %>%
+  dplyr::group_by(year) %>%
+  dplyr::summarise('min' = min(RPT_LOANS, na.rm = T),
+                   'P10' = quantile(RPT_LOANS, 0.10, na.rm = T),
+                   'Q1' = quantile(RPT_LOANS, 0.25, na.rm = T),
+                   'med' = quantile(RPT_LOANS, 0.50, na.rm = T),
+                   'iqr' = IQR(RPT_LOANS, na.rm =T),
+                   'mean' = mean(RPT_LOANS, na.rm = T),
+                   'std_dev' = sd(RPT_LOANS, na.rm = T),
+                   'Q3' = quantile(RPT_LOANS, 0.75, na.rm = T),
+                   'P90' = quantile(RPT_LOANS, 0.90, na.rm = T),
+                   'P95' = quantile(RPT_LOANS, 0.95, na.rm = T),
+                   'P99' = quantile(RPT_LOANS, 0.99, na.rm = T),
+                   'max' = max(RPT_LOANS, na.rm = T),
+                   'skew' = moments::skewness(RPT_LOANS, na.rm = T),
+                   'kurt' = moments::kurtosis(RPT_LOANS, na.rm = T))
+
+### By industry classification ###
+
+data_rpt_loans_summary_ind <- data_RPT_2 %>%
+  dplyr::group_by(NIC_2) %>%
+  dplyr::summarise('min' = min(RPT_LOANS, na.rm = T),
+                   'P10' = quantile(RPT_LOANS, 0.10, na.rm = T),
+                   'Q1' = quantile(RPT_LOANS, 0.25, na.rm = T),
+                   'med' = quantile(RPT_LOANS, 0.50, na.rm = T),
+                   'iqr' = IQR(RPT_LOANS, na.rm =T),
+                   'mean' = mean(RPT_LOANS, na.rm = T),
+                   'std_dev' = sd(RPT_LOANS, na.rm = T),
+                   'Q3' = quantile(RPT_LOANS, 0.75, na.rm = T),
+                   'P90' = quantile(RPT_LOANS, 0.90, na.rm = T),
+                   'P95' = quantile(RPT_LOANS, 0.95, na.rm = T),
+                   'P99' = quantile(RPT_LOANS, 0.99, na.rm = T),
+                   'max' = max(RPT_LOANS, na.rm = T),
+                   'skew' = moments::skewness(RPT_LOANS, na.rm = T),
+                   'kurt' = moments::kurtosis(RPT_LOANS, na.rm = T))
+
+### By BG classification ###
+
+data_rpt_loans_summary_BG <- data_RPT_2 %>%
+  dplyr::group_by(BG) %>%
+  dplyr::summarise('min' = min(RPT_LOANS, na.rm = T),
+                   'P10' = quantile(RPT_LOANS, 0.10, na.rm = T),
+                   'Q1' = quantile(RPT_LOANS, 0.25, na.rm = T),
+                   'med' = quantile(RPT_LOANS, 0.50, na.rm = T),
+                   'iqr' = IQR(RPT_LOANS, na.rm =T),
+                   'mean' = mean(RPT_LOANS, na.rm = T),
+                   'std_dev' = sd(RPT_LOANS, na.rm = T),
+                   'Q3' = quantile(RPT_LOANS, 0.75, na.rm = T),
+                   'P90' = quantile(RPT_LOANS, 0.90, na.rm = T),
+                   'P95' = quantile(RPT_LOANS, 0.95, na.rm = T),
+                   'P99' = quantile(RPT_LOANS, 0.99, na.rm = T),
+                   'max' = max(RPT_LOANS, na.rm = T),
+                   'skew' = moments::skewness(RPT_LOANS, na.rm = T),
+                   'kurt' = moments::kurtosis(RPT_LOANS, na.rm = T))
+
+
 
 ##############################################################################
 ########### Plots with time ##################################################
@@ -227,13 +447,6 @@ plot_box_RPT_agg_nonBG <- ggplot(dplyr::filter(data_RPT_2, BG == 0),
 #### Aggregate RPT boxplots with industries, classifications etc. ###
 #####################################################################
 
-# How many observations for each industry classification (NIC_2)
-ind_freq <- data_RPT_2 %>%
-  dplyr::group_by(NIC_2) %>%
-  dplyr::count() %>%
-  dplyr::arrange(desc(n)) %>%
-  dplyr::filter(n >= 400)
-
 # Make boxplots for each industry
 # With outliers
 plot_box_RPT_agg_industry <- ggplot(data_RPT_2 %>% filter(NIC_2 %in% ind_freq$NIC_2), 
@@ -245,6 +458,28 @@ plot_box_RPT_agg_industry_out_no <- ggplot(data_RPT_2 %>% filter(NIC_2 %in% ind_
                                     aes(x = NIC_2, y = RPT_agg)) +
   ylim(0, 0.5) +
   geom_boxplot(outlier.shape = NA)
+
+
+#####################################################################
+####### Plots with RPT types ########################################
+#####################################################################
+
+plot_med_rpt_loans_sales_agg <- ggplot(data = data_RPT_summary_TS, 
+                                       mapping = aes(x = year, y = med)) +
+  geom_point() +
+  geom_line(linetype = 'solid') +
+  geom_line(data = data_rpt_loans_summary_TS,
+            mapping = aes(x = year, y = med),
+            linetype = 'longdash') +
+  geom_point(data = data_rpt_loans_summary_TS,
+             mapping = aes(x = year, y = med)) +
+  # geom_point() +
+  # geom_line(data_rpt_sales_summary_TS, 
+  #           aes(x = year, y = med), 
+  #           linetype = 'dotdash') +
+  scale_x_continuous(breaks = year_breaks) +
+  geom_vline(xintercept = 2014, linetype = 'dashed') +
+  labs(x = '', y = "Median firm's RPT level")
 
 
 
